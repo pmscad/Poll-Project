893d827b2af6fffd481a4ffe982572490457dc61
Vikramjeet Multani <38405022+pmscad@users.noreply.github.com>
2020-01-21 11:24:43 +0000

    Html routes 1 (#39)
    * more html routes addedd
    
    * All routes configured
    
    Co-authored-by: Simona D <s.c.dulgheru@gmail.com>

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-const express = require('express');
+const express = require("express");
 require(`dotenv`).config(); // eslint-disable-line
 
 const PORT = process.env.PORT || 8080;
-const db = require('./models');
-const seeds = require(`./seed.js`)
+const db = require("./models");
+const seeds = require(`./seed.js`);
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// require('./routes/html-routes.js')(app);
-require('./routes/api-routes.js')(app);
+const htmlRoutes = require("./routes/html-routes.js");
+app.use(htmlRoutes);
+// require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-db.sequelize.sync({force:true})
-    .then(() => {
-        seeds(db);
-        app.listen(PORT, () => {
-            console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
-    });
-})   
-
+db.sequelize.sync({ force: true }).then(() => {
+  seeds(db);
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
