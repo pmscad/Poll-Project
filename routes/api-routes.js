fd6e9c660093fa273bd3b5eb88b6d5dc0d97245b
Dcox270891 <55593346+Dcox270891@users.noreply.github.com>
2020-01-24 14:12:20 +0000

    create poll now redirects to the correct answer page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -76,7 +76,7 @@ module.exports = function(app){
                     answer: answers
                 })
             })
-        res.end();
+            res.send({newPollId})
         })
     })
 
